a23beb6deabe883341df3339691a0b5ecf40b4dc
caoruchen <caoruchen@stu.ouc.edu.cn>
2024-12-17 17:57:57 +0800

    custom exceptions and server project tested

---
+++ luffycityapi/luffycityapi/apps/home/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+import logging
+logger = logging.getLogger("django")
+
+# Create your views here.
+class TestView(APIView):
+
+    def get(self, request):
+        logger.info("GET request received.")
+        1 / 0
+        return Response("Test view returned something.")
