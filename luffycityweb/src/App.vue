cc1ed20f22b1def7a287aa1e96975bb473ddec3d
caoruchen <caoruchen@stu.ouc.edu.cn>
2024-12-19 18:03:05 +0800

    feature: Header and Footer component added

--- luffycityweb/src/App.vue
+++ luffycityweb/src/App.vue
@@ -4,6 +4,8 @@
 
 <script setup>
 
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style scoped>
--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -1,12 +1,12 @@
 <script setup>
 
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
-<h1>Home</h1>
-  <div>
-    <el-button type="primary">Primary</el-button>
-  </div>
+<Header></Header>
+  <Footer></Footer>
 </template>
 
 <style scoped>
