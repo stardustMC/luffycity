8c4fba9dd32cedc6a526d0b0b0f43124cb3f73f3
caoruchen <caoruchen@stu.ouc.edu.cn>
2025-03-27 20:50:48 +0800

    feature: celery auto-start enabled

--- luffycityapi/luffycityapi/settings/dev.py
+++ luffycityapi/luffycityapi/settings/dev.py
@@ -386,15 +386,3 @@ CELERYD_TIME_LIMIT = 10 * 60
 CELERY_DISABLE_RATE_LIMITS = True
 # celery的任务结果内容格式
 CELERY_ACCEPT_CONTENT = ['json', 'pickle']
-
-# 之前定时任务（定时一次调用），使用了apply_async({}, countdown=30);
-# 设置定时任务（定时多次调用）的调用列表，需要单独运行SCHEDULE命令才能让celery执行定时任务：celery -A mycelery.main beat，当然worker还是要启动的
-# https://docs.celeryproject.org/en/stable/userguide/periodic-tasks.html
-from celery.schedules import crontab
-CELERY_BEAT_SCHEDULE = {
-    "user-add": {  # 定时任务的注册标记符[必须唯一的]
-        "task": "add",   # 定时任务的任务名称
-        "schedule": 10,  # 定时任务的调用时间，10表示每隔10秒调用一次add任务
-        # "schedule": crontab(hour=7, minute=30, day_of_week=1),,  # 定时任务的调用时间，每周一早上7点30分调用一次add任务
-    }
-}
